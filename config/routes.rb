ab01cefa3044c6295e42ee47900ccc386af703bf
KatrinaFrost <katrinalfrost@hotmail.com>
2017-08-29 11:52:16 +1000

    6th commit - website navigation

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,9 @@ Rails.application.routes.draw do
 
   get '/profile' => 'profile#index'
   get '/profile/create' => 'profile#create'
+  post 'profile/create' => 'profile#index'
+
+  get '/pages/feed' => 'pages#feed'
+
 
 end
