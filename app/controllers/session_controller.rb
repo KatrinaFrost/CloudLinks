cd2ded794ca6a9d805e84d674f942f533dd071f5
KatrinaFrost <katrinalfrost@hotmail.com>
2017-08-29 17:19:06 +1000

    7th commit - password saving again and buttons works

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -11,10 +11,10 @@ class SessionController < ApplicationController
       # If a user record with the entered in the form is present AND the user is authenticated (using bcrypt's authenticate method and the password entered in the form), store their id in the session hash and redirect them to the root path.
       session[:user_id] = user.id
       flash[:notice] = "User created!"
-      redirect_to root_path
+      redirect_to profile_path
     else
       # If the user cannot be authenticated, redirect them to the login_path.
-      flash[:error] = "User could not be created!"
+      flash[:error] = "Incorrect user name or password!"
       redirect_to login_path
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   # ------------- Log in ------------------- #
   get '/login' => 'session#new'
-  post '/login' => 'profile#index'
+  post '/login' => 'session#create'
   delete '/login' => 'session#destroy'
 
   # ------------- Profile ------------------- #
