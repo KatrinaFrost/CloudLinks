85c03f6aba1b58b6395ea3c4710d2e5e0ec76651
KatrinaFrost <katrinalfrost@hotmail.com>
2017-08-29 15:34:39 +1000

    commit 6

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -12,8 +12,10 @@ class LinksController < ApplicationController
   end
 
   def create
-    Link.create(link_params)
+    # Link.create(link_params)
     # redirect_to links_path
+    link = Link.create :link_display_name => params[:link][:link_display_name], :display_description => params[:link][:display_description]
+    redirect_to links_path
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,25 @@
 Rails.application.routes.draw do
 
+# ------------- Root to home ---------------- #
+
   root :to => 'pages#home'
 
+  # ------------- Resources ----------------- #
+  resources :users
+  resources :genres
+  resources :links
+  
+  # ------------- Session ------------------- #
   get '/login' => 'session#new'
   post '/login' => 'session#create'
   delete '/login' => 'session#destroy'
 
-  resources :users
-  resources :genres
-  resources :links
-
+  # ------------- Profile ------------------- #
   get '/profile' => 'profile#index'
   get '/profile/create' => 'profile#create'
-  post 'profile/create' => 'profile#index'
+  post '/profile/create' => 'profile#index'
 
+  # ------------- Feed ------------------- #
   get '/pages/feed' => 'pages#feed'
-
 
 end
