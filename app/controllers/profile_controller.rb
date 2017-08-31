7e2218e9e6c6d88ae2cbf7b6405e1509612e5226
KatrinaFrost <katrinalfrost@hotmail.com>
2017-09-01 08:40:35 +1000

    Final push before presentation

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -4,10 +4,8 @@ class ProfileController < ApplicationController
     @links = Link.all
     @genres = Genre.all
     @users = User.all
-    if params[:genre_id]
+    if @genres and params[:genre_id]
       @genre = Genre.find params[:genre_id]
-    else
-      @genre = Genre.find 1
     end
   end
 end
